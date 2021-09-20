87aa91c6597b8316cfb2bd5f5056506f79b546a8
R0merol3 <farisfrontiers@gmail.com>
2021-09-20 10:30:09 +0700

    Group numpad position

--- main.py
+++ main.py
@@ -25,20 +25,22 @@ class Program:
         self.rect = text.Rect()
         self.text = text.Text()
 
-        self.confirm_button = ConfirmButton("C", 30, 30, (680, 335), GREEN, D_GREEN)
-        self.reset_button = ResetButton("X", 30, 30, (600, 335), RED, D_RED)
         self.restart_button = RestartButton("R", 30, 30, (10, 10), BLUE, D_BLUE)
 
-        self.button1 = Button("1", 30, 30, (600, 200), L_GREY, GREY)
-        self.button2 = Button("2", 30, 30, (640, 200), L_GREY, GREY)
-        self.button3 = Button("3", 30, 30, (680, 200), L_GREY, GREY)
-        self.button4 = Button("4", 30, 30, (600, 245), L_GREY, GREY)
-        self.button5 = Button("5", 30, 30, (640, 245), L_GREY, GREY)
-        self.button6 = Button("6", 30, 30, (680, 245), L_GREY, GREY)
-        self.button7 = Button("7", 30, 30, (600, 290), L_GREY, GREY)
-        self.button8 = Button("8", 30, 30, (640, 290), L_GREY, GREY)
-        self.button9 = Button("9", 30, 30, (680, 290), L_GREY, GREY)
-        self.button0 = Button("0", 30, 30, (640, 335), L_GREY, GREY)
+        self.set_numpad_pos(0, 0)
+
+        self.button1 = Button("1", 30, 30, (680, 215), L_GREY, GREY)
+        self.button2 = Button("2", 30, 30, (720, 215), L_GREY, GREY)
+        self.button3 = Button("3", 30, 30, (760, 215), L_GREY, GREY)
+        self.button4 = Button("4", 30, 30, (680, 260), L_GREY, GREY)
+        self.button5 = Button("5", 30, 30, (720, 260), L_GREY, GREY)
+        self.button6 = Button("6", 30, 30, (760, 260), L_GREY, GREY)
+        self.button7 = Button("7", 30, 30, (680, 305), L_GREY, GREY)
+        self.button8 = Button("8", 30, 30, (720, 305), L_GREY, GREY)
+        self.button9 = Button("9", 30, 30, (760, 305), L_GREY, GREY)
+        self.button0 = Button("0", 30, 30, (720, 350), L_GREY, GREY)
+        self.reset_button = ResetButton("X", 30, 30, (680, 350), RED, D_RED)
+        self.confirm_button = ConfirmButton("C", 30, 30, (760, 350), GREEN, D_GREEN)
 
         self.text_box = text.TextBox(110, 30, (600, 155), BLACK, WHITE)
 
@@ -49,9 +51,17 @@ class Program:
         self.rect.draw_ui_rect(screen)
         self.text.draw_text(screen)
 
+        self.draw_numpad(screen)
+
+        self.restart_button.draw(screen)
+        
+
+        # Enter experimental functions here
+    
+    def draw_numpad(self, screen):
+        self.text_box.draw(screen)
         self.confirm_button.draw(screen)
         self.reset_button.draw(screen)
-        self.restart_button.draw(screen)
 
         self.button1.draw(screen)
         self.button2.draw(screen)
@@ -64,9 +74,27 @@ class Program:
         self.button9.draw(screen)
         self.button0.draw(screen)
 
-        self.text_box.draw(screen)
+    def set_numpad_pos(self, initial_x, initial_y):
+        grid_x = []
+        grid_y = []
+        grid_x.append(initial_x)
+        grid_y.append(initial_y)
+        dynamic_x = initial_x
+        dynamic_y = initial_y
+        column_count = 3
+        row_count = 5
+        x_space = 40
+        y_space = 45
 
-        # Enter experimental functions here
+        for x in range(column_count - 1):
+            grid_x.append(dynamic_x + x_space)
+            dynamic_x += x_space
+        for y in range(row_count - 1):
+            grid_y.append(dynamic_y + y_space)
+            dynamic_y += y_space
+
+        print(f"grid_x: {grid_x}")
+        print(f"grid_y: {grid_y}")
 
 
 # Executables -------------------------------------------------------------
