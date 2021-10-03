f8eb61772f8957663f230aac9a2912fd420c95f0
Faris Faikar Razannafi <farisfrontiers@gmail.com>
2021-10-03 18:39:01 +0700

    Recolor the game using retro color palette. Added CRT effect. Placed elements of constantvar.py into globalvar.py.

---
+++ code/button.py
@@ -0,0 +1,114 @@
+import pygame
+import numbergame
+import timer
+import globalvar as gv
+
+
+class Button:
+    def __init__(self, text, width, height, pos, top_color):
+        # Core attributes
+        self.pressed = False
+        self.elevation = 4
+        self.dynamic_elevation = self.elevation
+        self.original_y_pos = pos[1]
+        self.text = text
+        self.pos = pos
+
+        # top rectangle
+        self.top_rect = pygame.Rect(pos, (width, height))
+        self.TOP_COLOR = top_color
+        self.top_color = top_color
+
+        # bottom rectangle
+        self.bottom_rect = pygame.Rect(pos, (width, height))
+        self.bottom_color = gv.PURPLE
+
+        # hitbox rectangle
+        self.hitbox_rect = pygame.Rect(pos, (width, height))
+
+        # text
+        gui_font = pygame.font.Font(gv.chary_font, 20)
+        self.text_surf = gui_font.render(self.text, True, gv.PURPLE)
+        self.text_rect = self.text_surf.get_rect(center=self.top_rect.center)
+
+    def draw(self, screen):
+        # elevation logic
+        self.top_rect.y = self.original_y_pos - self.dynamic_elevation
+        self.text_rect.center = self.top_rect.center
+        self.hitbox_rect.y = self.original_y_pos - self.elevation
+
+        self.bottom_rect.midtop = self.top_rect.midtop
+        self.bottom_rect.height = self.top_rect.height + self.dynamic_elevation
+
+        pygame.draw.rect(screen, self.bottom_color, self.bottom_rect, border_radius=5)
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius=5)
+
+        screen.blit(self.text_surf, self.text_rect)
+        self.check_click()
+
+    def check_click(self):
+        mouse_pos = pygame.mouse.get_pos()
+
+        if self.hitbox_rect.collidepoint(mouse_pos):
+            self.top_color = gv.WHITE
+            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+                self.dynamic_elevation = 0
+                self.pressed = True
+            else:
+                self.dynamic_elevation = self.elevation
+                if self.pressed is True:
+                    self.button_action()
+                    self.pressed = False
+        else:
+            self.pressed = False
+            self.dynamic_elevation = self.elevation
+            self.top_color = self.TOP_COLOR
+
+    def button_action(self):
+        user_input_length = len(gv.num_input)
+        if user_input_length < 4:
+            if gv.num_input.find(self.text) == -1:
+                gv.num_input += self.text
+                gv.error_message = ""
+            else:
+                gv.error_message = "You can only input different numbers"
+        else:
+            gv.error_message = "You can only input up to 4 digits"
+
+
+class ConfirmButton(Button):
+    def button_action(self):
+        if len(gv.num_input) == 4:
+            if gv.game_state == 'main_game':
+                numbergame.compare()
+            gv.num_input = ""
+            gv.error_message = ""
+        else:
+            gv.error_message = "You need to input 4 numbers"
+
+
+class ResetButton(Button):
+    def button_action(self):
+        gv.num_input = ""
+
+
+class RestartButton(Button):
+    def button_action(self):
+        numbergame.restart()
+        timer.reset_timer()
+        gv.game_state = 'main_game'
+
+        # error_message shall be eradicated
+        gv.error_message = ""
+
+
+class HighscoreButton(Button):
+    def button_action(self):
+        timer.stop_timer()
+        gv.game_state = 'highscore'
+
+
+class AchievementButton(Button):
+    def button_action(self):
+        print("Display Achievements")
+        gv.game_state = 'achievement'
---
+++ code/main.py
@@ -0,0 +1,164 @@
+import pygame
+import sys
+
+from random import randint
+from text import Text, TextBox
+from button import Button, ConfirmButton, ResetButton, RestartButton, HighscoreButton, AchievementButton
+import timer
+import highscore as hs
+import globalvar as gv
+
+
+class Program:
+    def __init__(self):
+        # Enter instances here -----------------------------------------------------
+        self.text = Text()
+
+        self.restart_button = RestartButton("Restart", 110, 30, (680, screen_height - 40), gv.RED)
+        self.highscore_button = HighscoreButton("Highscore", 110, 30, (680, screen_height - 80), gv.BLUE)
+        self.achievement_button = AchievementButton("Achievement", 110, 30, (680, screen_height - 120), gv.LIME)
+
+        self.grid_pos = []
+        self.set_numpad_pos(680, 40)
+
+        # Enter the coordinates using -> grid_pos[x][y]
+        self.text_box = TextBox(110, 30, (self.grid_pos[0][0]), gv.PURPLE, gv.WHITE)
+        self.button1 = Button("1", 30, 30, (self.grid_pos[0][1]), gv.ORANGE)
+        self.button2 = Button("2", 30, 30, (self.grid_pos[1][1]), gv.ORANGE)
+        self.button3 = Button("3", 30, 30, (self.grid_pos[2][1]), gv.ORANGE)
+        self.button4 = Button("4", 30, 30, (self.grid_pos[0][2]), gv.ORANGE)
+        self.button5 = Button("5", 30, 30, (self.grid_pos[1][2]), gv.ORANGE)
+        self.button6 = Button("6", 30, 30, (self.grid_pos[2][2]), gv.ORANGE)
+        self.button7 = Button("7", 30, 30, (self.grid_pos[0][3]), gv.ORANGE)
+        self.button8 = Button("8", 30, 30, (self.grid_pos[1][3]), gv.ORANGE)
+        self.button9 = Button("9", 30, 30, (self.grid_pos[2][3]), gv.ORANGE)
+        self.button0 = Button("0", 30, 30, (self.grid_pos[1][4]), gv.ORANGE)
+        self.reset_button = ResetButton("X", 30, 30, (self.grid_pos[0][4]), gv.RED)
+        self.confirm_button = ConfirmButton("C", 30, 30, (self.grid_pos[2][4]), gv.LIME)
+
+        # Enter experimental instances here
+
+    def run(self):  # This bad boy runs every frame -------------------------------
+        # Enter functions here
+        timer.count_up()
+        self.draw_ui_rect()
+        self.text.draw_text(screen)
+
+        self.draw_numpad()
+        self.restart_button.draw(screen)
+        self.highscore_button.draw(screen)
+        self.achievement_button.draw(screen)
+
+        # Enter experimental functions here
+
+    @staticmethod
+    def draw_ui_rect():  # this will be replaced with a proper background... I think
+        pygame.draw.rect(screen, gv.CREAM, (670, 0, 280, 400))  # 'calculator' rect
+        pygame.draw.rect(screen, gv.GREEN, (670, 0, 130, 30))  # 'timer' rect
+        pygame.draw.rect(screen, gv.GREEN, (670, 260, 130, 140))  # 'timer' rect
+    
+    def draw_numpad(self):
+        self.text_box.draw(screen)
+        self.confirm_button.draw(screen)
+        self.reset_button.draw(screen)
+
+        self.button1.draw(screen)
+        self.button2.draw(screen)
+        self.button3.draw(screen)
+        self.button4.draw(screen)
+        self.button5.draw(screen)
+        self.button6.draw(screen)
+        self.button7.draw(screen)
+        self.button8.draw(screen)
+        self.button9.draw(screen)
+        self.button0.draw(screen)
+
+    def set_numpad_pos(self, initial_x, initial_y):
+        dynamic_x = initial_x
+        dynamic_y = initial_y
+        column_count = 3
+        row_count = 5
+        x_space = 40
+        y_space = 45
+
+        # Create a 3d list containing the button grid coordinates
+        for x in range(column_count):
+            self.grid_pos.append([])
+            for y in range(row_count):
+                self.grid_pos[x].append([dynamic_x, dynamic_y])
+                dynamic_y += y_space
+            dynamic_y = initial_y
+            dynamic_x += x_space
+
+
+class CRT:
+    def __init__(self):
+        self.tv_width = 670
+        self.tv = pygame.image.load('sprites/tv.png').convert_alpha()
+        self.tv = pygame.transform.scale(self.tv, (self.tv_width, screen_height))
+
+    def create_crt_lines(self):
+        line_height = 3
+        line_amount = int(screen_height / line_height)
+        for line in range(line_amount):
+            y_pos = line * line_height
+            pygame.draw.line(self.tv, 'black', (0, y_pos), (self.tv_width, y_pos), 1)
+
+    def draw(self):
+        self.tv.set_alpha(randint(60, 90))
+        self.create_crt_lines()
+        screen.blit(self.tv, (0, 0))
+
+
+# Executables -------------------------------------------------------------
+screen_width = 800
+screen_height = 400
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+
+def main():
+    # Initialize pygame
+    pygame.init()
+    clock = pygame.time.Clock()
+    icon = pygame.image.load('sprites/number_game.png')
+
+    # Initiate instances
+    program = Program()
+    crt = CRT()
+
+    # Set caption, icon, color
+    pygame.display.set_caption("Number Game!")
+    pygame.display.set_icon(icon)
+
+    # Main loop (runs every tick) -------------------------------------------------
+    while True:
+        # Event code
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if gv.game_state == 'won':
+                    if event.key == pygame.K_RETURN:
+                        gv.game_state = 'highscore'
+                        hs.save_score()
+                    if event.key == pygame.K_BACKSPACE:
+                        gv.text_input = gv.text_input[:-1]
+                    else:
+                        if len(gv.text_input) < 5:
+                            gv.text_input += event.unicode
+
+        # Game code
+        program.run()
+        crt.draw()
+
+        # Updates, mind the order
+        pygame.display.flip()
+        screen.fill(gv.PURPLE)
+
+        # Time & Clock
+        clock.tick(60)
+
+
+if __name__ == '__main__':
+    main()
