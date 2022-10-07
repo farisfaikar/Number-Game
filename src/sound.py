13fc52e2a7a37d9827cc12b9602c4899f30ee157
Brandon Bowdoin <67806039+spaghettiosareyummy@users.noreply.github.com>
2022-10-07 17:11:32 -0500

    Compressed the WAV files

--- src/sound.py
+++ src/sound.py
@@ -17,14 +17,14 @@ class Sound:
         else:
             random_boot_up = boot_up_sound[random.randint(1, len(boot_up_sound) - 1)]
 
-        pygame.mixer.music.load("sound/server_ambient.wav")
-        pygame.mixer.music.set_volume(0.5)
+        mixer.music.load("sound/server_ambient.wav")
+        mixer.music.set_volume(0.5)
         self.boot_up = mixer.Sound(random_boot_up)
         self.boot_up.set_volume(0.7)
 
     @staticmethod
     def play_ambient():
-        pygame.mixer.music.play(-1)
+        mixer.music.play(-1)
 
     def play_boot_up(self):
         self.boot_up.play()
