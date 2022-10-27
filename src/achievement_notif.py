921d81c2b1b1611da730dfc2e1d88032697b46ef
Ujjwal Aggarwal <96613056+UjjwalAggarwal-1@users.noreply.github.com>
2022-10-27 06:43:07 +0530

    continue box in message box (#56)
    * achievement message done
    
    * newlines
    
    * black
    
    * messagebox: continue button
    
    * edits: color, grammar
    
    * error fixed: hangs on getting achivement
    
    Co-authored-by: Faris Faikar <farisfrontiers@gmail.com>

--- src/achievement_notif.py
+++ src/achievement_notif.py
@@ -1,5 +1,7 @@
 from tkinter import *
+
 from PIL import Image, ImageTk
+
 from globalvar import ACHIEVEMENT_LIST
 
 
@@ -11,14 +13,14 @@ def achv_notif(index: int):
 
     ## style
     bg_c = "#2d162d"  ### c for color
-    head_c = "YELLOW"
+    head_c = "#f7f4c4"
     head_style = ("Charybdis", 17, "bold")
-    achv_c = "WHITE"
+    achv_c = "#f19d5d"
     achv_style = ("Charybdis", 15)
 
     ## main window
     main = Tk()
-    main.title("Congratulations")
+    main.title("New Achievement")
     main.iconbitmap("sprite/number_game.ico")
     main.geometry(f"{wd}x{hg}")
     main.minsize(wd, hg)
@@ -27,15 +29,17 @@ def achv_notif(index: int):
     ## background
     back = Frame(main)
     back.pack()
-    bgimg = ImageTk.PhotoImage(Image.open("sprite/tv.png").resize((wd, hg)))
+    bgimg = ImageTk.PhotoImage(
+        Image.open("sprite/tv.png").resize((wd, hg)), master=back
+    )
     limg = Label(back, i=bgimg)
     limg.config(bg=bg_c)  # hex code for the PURPLE used in bg game screen
     limg.grid(column=0, row=0, columnspan=3, rowspan=3)
 
     ## message
-    mesg = Label(back, text="You have unlocked a new achievement !", bg=bg_c, fg=head_c)
+    mesg = Label(back, text="Congratulations !", bg=bg_c, fg=head_c)
     mesg.configure(font=head_style)
-    mesg.grid(column=1, row=0)
+    mesg.grid(column=0, row=0, columnspan=3)
 
     mesg = Label(
         back,
@@ -46,10 +50,31 @@ def achv_notif(index: int):
         fg=achv_c,
     )
     mesg.configure(font=achv_style)
-    mesg.grid(column=1, row=1)
+    mesg.grid(column=0, row=1, columnspan=3)
+
+    ## ok button
+    button = Button(back, text="Continue", command=main.destroy)
+    button.configure(
+        activebackground="DARKGREEN",
+        activeforeground="GREY",
+        bg="#74a441",
+        fg="WHITE",
+        borderwidth=3,
+        overrelief="groove",
+        state="normal",
+        relief="ridge",
+        takefocus=0,
+        font=("Charybdis", 12, "bold"),
+    )
+    button.grid(row=2, column=2)
 
     main.mainloop()
 
 
+def all_achievements(list_ach):
+    for a in list_ach:
+        achv_notif(a)
+
+
 if __name__ == "__main__":
-    achv_notif(1)
+    achv_notif(2)
